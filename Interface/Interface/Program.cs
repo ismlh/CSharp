9453c802afed3b8f9ce288b4e6e2adacb96011c1
Islam <93212825+ismlh@users.noreply.github.com>
2023-07-06 18:16:26 -0700

    A real example of applying interface in C#
    Interface Example

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cashier cashier = new Cashier(new cash());
+            cashier.Checkout(100m);
+            Cashier cashier1 = new Cashier(new Visa());
+            cashier1.Checkout(100m);
+            Cashier cashie2r = new Cashier(new masterCard());
+            cashie2r.Checkout(100m);
+        }
+    }
+     class Cashier
+    {
+        private IPayment _payment;
+        public Cashier(IPayment payment)
+        {
+            this._payment = payment;
+        }
+        public void Checkout(decimal amount)
+        {
+            _payment.pay(amount);
+        }
+    }
+    interface IPayment
+    {
+        void pay(decimal amount);
+    }
+    class cash : IPayment
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"You will pay {amount} using {GetType().ToString().Replace("Interface.","")}");
+        }
+    }
+    class Visa : IPayment
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"You will pay {amount} using {GetType().ToString().Replace("Interface.", "")}");
+        }
+    }
+    class masterCard : IPayment
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"You will pay {amount} using {GetType().ToString().Replace("Interface.", "")}");
+        }
+    }
+}
