e8023718719b4fa5f13994b4943733ae58f739c2
Islam <93212825+ismlh@users.noreply.github.com>
2023-07-06 17:35:45 -0700

    Add files via upload
    Delegate Example

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emps = new Employee[]
+           {
+                new Employee{Id=1,Name="Islam",Age=27,Gender='M' },
+                new Employee{Id=2,Name="Ali",Age=26,Gender='M' },
+                new Employee{Id=3,Name="Marwan",Age=19,Gender='M' },
+                new Employee{Id=4,Name="Marwa",Age=20,Gender='f' },
+                new Employee{Id=5,Name="Meran",Age=21,Gender='f' },
+                new Employee{Id=6,Name="Salwa",Age=23,Gender='f' },
+           };
+            Report report = new Report();
+            #region Old Way 
+            //report.AgeEqual20OrLessThan20(emps);
+            //report.AgeBetween21And25(emps);
+            //report.AgeCreaterThan25(emps);
+            #endregion
+            report.EmpsAge(emps, "Age < = 20", e => e.Age <= 20);
+            report.EmpsAge(emps, "Age < 20 And < = 25", e => e.Age > 20&&e.Age<=25);
+            report.EmpsAge(emps, "Age >  25", e => e.Age > 25);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Delegate/Delegate/Report.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Delegate
+{
+    public  class Report
+    {
+        //delegate
+        public delegate bool AgeCondations(Employee employees);
+        public void EmpsAge(Employee[] employee,string Title,AgeCondations condation)
+        {
+            System.Console.WriteLine(Title);
+            System.Console.WriteLine("----------------------------");
+            foreach (var e in employee)
+            {
+                if (condation(e))
+                    Console.WriteLine($"Id:{e.Id} Name:{e.Name} Age:{e.Age} Gender:{e.Gender}");
+            }
+            Console.WriteLine("\n\n");
+        }
+        //#region Old Way
+        //public void AgeEqual20OrLessThan20(Employee[] employee)
+        //{
+        //    System.Console.WriteLine("Age Equal 20 Or Less Than 20");
+        //    System.Console.WriteLine("----------------------------");
+        //    foreach (var e in employee)
+        //    {
+        //        if (e.Age <= 20)
+        //            Console.WriteLine($"Id:{e.Id} Name:{e.Name} Age:{e.Age} Gender:{e.Gender}");
+        //    }
+        //    Console.WriteLine("\n\n");
+        //}
+        //public void AgeBetween21And25(Employee[] employee)
+        //{
+        //    System.Console.WriteLine("Age Betwween 21 And 25");
+        //    System.Console.WriteLine("----------------------------");
+        //    foreach (var e in employee)
+        //    {
+        //        if (e.Age > 20 && e.Age <= 25)
+        //            Console.WriteLine($"Id:{e.Id} Name:{e.Name} Age:{e.Age} Gender:{e.Gender}");
+        //    }
+        //    Console.WriteLine("\n\n");
+        //}
+        //public void AgeCreaterThan25(Employee[] employee)
+        //{
+        //    System.Console.WriteLine("Age Creater than  25");
+        //    System.Console.WriteLine("----------------------------");
+        //    foreach (var e in employee)
+        //    {
+        //        if (e.Age > 25)
+        //            Console.WriteLine($"Id:{e.Id} Name:{e.Name} Age:{e.Age} Gender:{e.Gender}");
+        //    }
+        //}
+        //#endregion
+    }
+}
