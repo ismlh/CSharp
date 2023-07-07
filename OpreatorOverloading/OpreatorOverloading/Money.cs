ae18d9c4c499cf3afdced0b4b21e3c1e1e01d0db
Islam <islamblal648@gmail.com>
2023-07-06 19:28:03 -0700

    C# Basics Solutions

---
+++ OpreatorOverloading/OpreatorOverloading/Money.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace OpreatorOverloading
+{
+    //Opreator Overloading
+    //// Mean That I Make User Defined Type(class) And Want To Make Opreators(+,-,/,*,++,--,>,<,==) on It
+    class Money
+    {
+        private decimal _amount;
+        public decimal Amount => this._amount;
+        public Money(decimal value)
+        {
+            this._amount = Math.Round(value, 2);
+        }
+        public static Money operator +(Money money1, Money money2)
+        {
+            var value = money1.Amount + money2.Amount;
+            return new Money(value);
+        }
+        public static Money operator -(Money money1,Money money2)
+        {
+            var value = money1.Amount - money2.Amount;
+            return new Money(value);
+        }
+        public static Money operator *(Money money,Money money1)
+        {
+            var value = money.Amount * money1.Amount;
+            return new Money(value);
+        }
+        public static Money operator /(Money money, Money money1)
+        {
+            var value = money.Amount / money1.Amount;
+            return new Money(value);
+        }
+        public static bool operator >(Money money,Money money1)
+        {
+            return money.Amount > money1.Amount;
+        }
+        public static bool operator <(Money money, Money money1)
+        {
+            return money.Amount < money1.Amount;
+        }
+        public static bool operator ==(Money money, Money money1)
+        {
+            return money.Amount == money1.Amount;
+        }
+        public static bool operator !=(Money money, Money money1)
+        {
+            return money.Amount != money1.Amount;
+        }
+        public static Money operator ++(Money money)
+        {
+            var value = money._amount++;
+            return new Money(value);
+        }
+        public static Money operator --(Money money)
+        {
+            var value = money._amount--;
+            return new Money(value);
+        }
+        //Now I have user defined type(Money) and i can done opreator on it 
+    }
+}
---
+++ OpreatorOverloading/OpreatorOverloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OpreatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new(20);
+            Money money1 = new(10);
+            Money money2 = money + money1;
+            Money money3 = money - money1;
+            Money money4 = money * money1;
+            Money money5 = money / money1;
+            var money6 = money > money1;
+            var money7 = money < money1;
+            var money8 = money == money1;
+            var money9 = money != money1;
+            Console.WriteLine($"m + m1 = {money2.Amount}");
+            Console.WriteLine($"m - m1 = {money3.Amount}");
+            Console.WriteLine($"m * m1 = {money4.Amount}");
+            Console.WriteLine($"m / m1 = {money5.Amount}");
+            Console.WriteLine($"m > m1 = {money6}");
+            Console.WriteLine($"m < m1 = {money7}");
+            Console.WriteLine($"m == m1 = {money8}");
+            Console.WriteLine($"m != m1 = {money9}");
+            Console.ReadKey();
+        }
+    }
+}
