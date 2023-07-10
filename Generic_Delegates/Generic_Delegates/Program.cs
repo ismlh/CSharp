8730668a1967e200c7c4eee3a3b47ded013f3115
Islam <93212825+ismlh@users.noreply.github.com>
2023-07-10 02:06:55 -0700

    Generic delegates (Action,Func,Predicate) In C#
    Generic delegates (Action,Func,Predicate) In C#

---
+++ Generic_Delegates/Generic_Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Delegates
+{
+    class Program
+    {
+        //public delegate bool Filter(int n);//Normal delegate
+       // public delegate bool Filter<in T>(T n);//Generic delegate that take generic T and return bool
+        //public delegate T1 Filter<in T,out T1>(T n);//Generic delegate that take generic T and return generic T1
+        static void Main(string[] args)
+        {
+            IEnumerable<int> list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Print(list, l => l < 6);
+            IEnumerable<double> list1 = new double[] { 1.1, 2.3, 3.5, 4.5, 5.6, 6.2, 7.3, 8.2, 9.1 };
+            Print(list1, l => l < 6.2);
+            Console.ReadKey();
+        }
+        static void Print<T>(IEnumerable<T> list,Predicate<T> filter)
+        {
+            foreach (var l in list)
+            {
+                if (filter(l))
+                    Console.WriteLine(l);
+            }
+        }
+    }
+}
---
+++ Generic_Delegates/bulit_In_Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// There is 3 Generic Delegates (action ,func,predicate)
+/// <summary>
+/// --Action : -Return Void
+///            -take 16 in parametares
+/// --func :   -Return generic(out)
+///            -take 16 in parametares
+/// --predicate : -Return bool
+///            -take 1 in parametares
+/// </summary>
+namespace bulit_In_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> action = Print;
+            action(1);
+            Func<int, int, int> func = Add;
+            Console.WriteLine(   func(1, 2));
+            Predicate<int> predicate = Check;
+            Console.WriteLine(predicate(1));
+            Console.ReadKey();
+        }
+        static void Print(int n) => Console.WriteLine(n);
+        static int Add(int n, int n1) => n + n1;
+        static bool Check(int n) => n >0;
+    }
+}
